48328e1d5ceb14db644705f4e2519df2f6720cf7
Josekappert1993 <jose_kappert1993@hotmail.com>
2020-03-09 14:18:00 +0100

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomepageComponent } from './homepage/homepage.component';
+const appRoutes: Routes = [
+  {
+    path: 'home', component:HomepageComponent
+  }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes, {enableTracing: true}),
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+  
+ }
